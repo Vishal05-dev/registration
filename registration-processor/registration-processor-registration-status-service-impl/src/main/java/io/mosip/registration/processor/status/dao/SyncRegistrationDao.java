4daef4e6162b513d5116601b1e1b6ff8c7609170
Vishal05-dev <vishalvenugopal00@gmail.com>
2026-05-08 16:33:00 +0530

    Add registration date constant and update filter logic
    Added a constant for registration date and updated filter handling to parse LocalDate for registration date.

--- registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/dao/SyncRegistrationDao.java
+++ registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/dao/SyncRegistrationDao.java
@@ -1,5 +1,6 @@
 package io.mosip.registration.processor.status.dao;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -64,6 +65,8 @@ public class SyncRegistrationDao {
 
 	/** The Constant ISDELETED_COLON. */
 	public static final String ISDELETED_COLON = ".isDeleted=:";
+
+	public static final String REGISTRATION_DATE = "registrationDate";
 
 
 	/**
@@ -181,10 +184,21 @@ public class SyncRegistrationDao {
 			while (searchIterator.hasNext()) {
 				FilterInfo filterInfo = searchIterator.next();
 				if (filterInfo.getType().equalsIgnoreCase("between")) {
-					sb.append(EMPTY_STRING + AND + EMPTY_STRING + alias + "." + filterInfo.getColumnName()
-							+ EMPTY_STRING + BETWEEN + "'" + filterInfo.getFromValue() + "'" + EMPTY_STRING + AND
-							+ EMPTY_STRING + "'" + filterInfo.getToValue() + "'");
+					String fromParam = filterInfo.getColumnName() + "_from";
+					String toParam = filterInfo.getColumnName() + "_to";
 
+					sb.append(EMPTY_STRING + AND + EMPTY_STRING + alias + "." + filterInfo.getColumnName()
+							+ " BETWEEN :" + fromParam + " AND :" + toParam);
+
+					if (REGISTRATION_DATE.equals(filterInfo.getColumnName())) {
+						params.put(fromParam,
+								LocalDate.parse(filterInfo.getFromValue()));
+						params.put(toParam,
+								LocalDate.parse(filterInfo.getToValue()));
+					} else {
+						params.put(fromParam, filterInfo.getFromValue());
+						params.put(toParam, filterInfo.getToValue());
+					}
 				} else {
 					sb.append(EMPTY_STRING + AND + EMPTY_STRING + alias + "." + filterInfo.getColumnName() + "=:"
 							+ filterInfo.getColumnName());
